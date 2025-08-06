7a5e8f59b4869529c285efa2164d3c0ddd3f21b5
RichardGualotuna <rfgualotuna@espe.edu.ec>
2025-08-06 11:45:00 -0500

    microservicio-facturacion

---
+++ microservicio-facturacion/microservicio-facturacion/src/main/java/microservicio_facturacion/MicroservicioFacturacionApplication.java
@@ -0,0 +1,13 @@
+package microservicio_facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioFacturacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioFacturacionApplication.class, args);
+	}
+
+}
